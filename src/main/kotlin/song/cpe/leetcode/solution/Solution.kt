6aff98386fab25388b6b20f70236d19ada9cf6ee
songrit maleerat <song.cpe@gmail.com>
2022-06-21 23:25:08 +0700

    Implement logic

--- src/main/kotlin/song/cpe/leetcode/solution/Solution.kt
+++ src/main/kotlin/song/cpe/leetcode/solution/Solution.kt
@@ -14,8 +14,68 @@ class Solution {
 
     inline fun convertToInteger(romanNumber: String) = RomanNumber.valueOf(romanNumber).value
 
-    fun romanToInt(s: String): Int {
-        println()
-        return 0
+    fun romanToInt(input: String): Int {
+        var inputArrays = input.toCharArray()
+        var sum = 0
+        var firstLengthIndex = 0
+        if (inputArrays.size == 1) {
+            return convertToInteger(inputArrays.get(0).toString())
+        }
+        for (i in 0..inputArrays.size - 1) {
+            val numStr = inputArrays.get(i).toString()
+            if (i == inputArrays.size - 1 && i == firstLengthIndex) {
+                sum += convertToInteger(inputArrays.get(i).toString())
+                continue
+            } else if (i < inputArrays.size - 1 && convertToInteger(inputArrays.get(i).toString()) > convertToInteger(
+                    inputArrays.get(i + 1).toString()
+                ) && i == firstLengthIndex
+            ) {
+                sum += convertToInteger(inputArrays.get(i).toString())
+                firstLengthIndex = i + 1
+                continue
+            } else if (inputArrays.get(firstLengthIndex).toString() != numStr || i == inputArrays.size - 1) {
+                if (convertToInteger(
+                        inputArrays.get(firstLengthIndex).toString()
+                    ) == convertToInteger(inputArrays.get(i).toString())
+                ) {
+                    var tempSum = 0
+                    for (j in firstLengthIndex..i) {
+                        tempSum += convertToInteger(inputArrays.get(j).toString())
+                    }
+                    sum += tempSum
+                } else if (convertToInteger(inputArrays.get(firstLengthIndex).toString()) >= convertToInteger(
+                        inputArrays.get(i).toString()
+                    )
+                ) {
+                    if (i < inputArrays.size - 1 && convertToInteger(inputArrays.get(i).toString()) < convertToInteger(
+                            inputArrays.get(i + 1).toString()
+                        )
+                    ) {
+                        var tempSum = 0
+                        for (j in firstLengthIndex..i - 1) {
+                            tempSum += convertToInteger(inputArrays.get(j).toString())
+                        }
+                        sum += tempSum
+                        firstLengthIndex = i
+                        continue
+                    } else {
+                        var tempSum = 0
+                        for (j in firstLengthIndex..i) {
+                            tempSum += convertToInteger(inputArrays.get(j).toString())
+                        }
+                        sum += tempSum
+                    }
+                } else {
+                    var tempSum = 0
+                    for (j in firstLengthIndex..i - 1) {
+                        tempSum += convertToInteger(inputArrays.get(j).toString())
+                    }
+                    sum += convertToInteger(inputArrays.get(i).toString()) - tempSum
+                }
+                firstLengthIndex = i + 1
+                continue
+            }
+        }
+        return sum
     }
 }
--- src/test/kotlin/song/cpe/leetcode/solution/SolutionTest.kt
+++ src/test/kotlin/song/cpe/leetcode/solution/SolutionTest.kt
@@ -1,15 +1,40 @@
 package song.cpe.leetcode.solution
 
+
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class SolutionTest {
 
     var solution: Solution = Solution()
 
-    @Test
-    fun romanToInt() {
-        solution.romanToInt("IV")
-        Assertions.assertTrue(true)
+    companion object {
+        @JvmStatic
+        fun inputs(): List<Arguments> {
+            return listOf(
+                Arguments.of("IV", 4),
+                Arguments.of("V", 5),
+                Arguments.of("I", 1),
+                Arguments.of("IVV", 9),
+                Arguments.of("III", 3),
+                Arguments.of("LVIII", 58),
+                Arguments.of("MCMXCIV", 1994),
+                Arguments.of("DCXXI", 621),
+                Arguments.of("MDCCCLXXXIV", 1884),
+
+            )
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    fun romanToInt(input: String, expectedValue: Int) {
+        val result = solution.romanToInt(input)
+        println("result: $result")
+        Assertions.assertEquals(expectedValue, result)
     }
 }
